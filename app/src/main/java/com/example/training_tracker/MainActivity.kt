84fad484ea378db1ae1b502bea3abbe29c8cdc96
DanHolmes752 <westfield1703@gmail.com>
2023-08-20 13:45:36 +0100

    Added start screen split buttons

--- app/src/main/java/com/example/training_tracker/MainActivity.kt
+++ app/src/main/java/com/example/training_tracker/MainActivity.kt
@@ -3,11 +3,15 @@ package com.example.training_tracker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.training_tracker.ui.theme.TrainingtrackerTheme
@@ -22,9 +26,28 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-
+                    ShowStartScreen()
                 }
             }
+        }
+    }
+}
+@Preview(showBackground = false, showSystemUi = true)
+@Composable
+fun ShowStartScreen(modifier: Modifier = Modifier) {
+    Column(
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally,
+        modifier = modifier
+    ) {
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Full Body Split")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Upper Lower Split")
+        }
+        Button(onClick = { /*TODO*/ }) {
+            Text(text = "Push, Pull Legs Split")
         }
     }
 }
